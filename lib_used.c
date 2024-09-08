50f594bb6df4ecd5eaab9a39e48833f238360de9
IliaPopovsky <popovskyilia@gmail.com>
2024-09-08 21:10:04 +0300

    третья версия проекта

--- lib_used.c
+++ lib_used.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 struct Weather {
     struct Current_condition {
@@ -36,6 +37,14 @@ struct Weather {
         char *windspeedKmph;
         char *windspeedMiles;
     } *current_condition;
+    struct Nearest_area {
+        struct AreaName {
+            char *value;
+        } *areaName;
+        struct Region {
+            char *value;
+        } *region;
+    } *nearest_area;
 };
 
 static void callback(enum NanoJSONCError error, const char *const key, const char *const value, const char *const parentKey, void *object) {
@@ -85,6 +94,29 @@ static void callback(enum NanoJSONCError error, const char *const key, const cha
         }
         if (strcmp(key, "value") == 0) (*weather)->current_condition->weatherDesc->value = strdup(value);
     }
+
+    if (strcmp(parentKey, "data[nearest_area][0]") == 0) {
+        if ((*weather)->nearest_area == NULL) {
+            (*weather)->nearest_area = malloc(sizeof(struct Nearest_area));
+            *(*weather)->nearest_area = (struct Nearest_area){0};
+        }
+    }
+
+    if (strcmp(parentKey, "data[nearest_area][0][areaName][0]") == 0) {
+        if ((*weather)->nearest_area->areaName == NULL) {
+            (*weather)->nearest_area->areaName = malloc(sizeof(struct AreaName));
+            *(*weather)->nearest_area->areaName = (struct AreaName){0};
+        }
+        if (strcmp(key, "value") == 0) (*weather)->nearest_area->areaName->value = strdup(value);
+    }
+
+    if (strcmp(parentKey, "data[nearest_area][0][region][0]") == 0) {
+        if ((*weather)->nearest_area->region == NULL) {
+            (*weather)->nearest_area->region = malloc(sizeof(struct Region));
+            *(*weather)->nearest_area->region = (struct Region){0};
+        }
+        if (strcmp(key, "value") == 0) (*weather)->nearest_area->region->value = strdup(value);
+    }
 }
 
 void weather_free(struct Weather **weather) {
@@ -92,6 +124,14 @@ void weather_free(struct Weather **weather) {
     (*weather)->current_condition->weatherDesc = NULL;
     free((*weather)->current_condition);
     (*weather)->current_condition = NULL;
+    /*
+    free((*weather)->nearest_area->areaName);
+    (*weather)->nearest_area->areaName = NULL;
+    */
+    free((*weather)->nearest_area->region);
+    (*weather)->nearest_area->region = NULL;
+    free((*weather)->nearest_area);
+    (*weather)->nearest_area = NULL;
     free(*weather);
     *weather = NULL;
 }
@@ -105,12 +145,13 @@ int main(int argc, char *argv[])
        char pogoda_Paris[] = "curl -sLo file9.txt https://wttr.in/Paris?format=j1";
        char enter_gorod[] = "curl -sLo file10.txt https://wttr.in/London?format=j1";
        char *moscow[] = {"curl", "-sLo Moscow.json https://wttr.in/Moscow?format=j1", 0};
+       char moscow1[] = "curl -sLo Moscow.json https://wttr.in/Moscow?format=j1";
        system("help");
        system("cd");
        //system("CHCP 1251");
        system("CHCP ");
-       system("cd C:");
-       system("cd /D G:\English");
+
+
        //system("cmd");
        //system("help cd");
        //system("CMD");
@@ -134,7 +175,7 @@ int main(int argc, char *argv[])
        char *args2[] = {"curl", "-sLo file13.txt https://wttr.in/Paris?format=j1", 0};
        //execvp("echo", args);
        //printf("execvp() failed. Error: %s\n", strerror(errno));
-       system("path");
+       system(moscow1);
        //execvp("curl -sLo", args1);
        //printf("execvp() failed. Error: %s\n", strerror(errno));
        //execvp("C:\Windows\System32\curl.exe", args1);
@@ -193,23 +234,82 @@ int main(int argc, char *argv[])
 
     struct Weather *weather = NULL;
     nanojsonc_parse_object(buffer, "data", &weather, callback);
-    //verify(weather);
 
-    #if 0
-    //printf("%s %s %d\n", person->name->first, person->name->last, person->age);
 
-    person_free(&weather);
-    #endif // 0
-    printf("%s %s %s %sKmph\n", weather->current_condition->weatherDesc->value, weather->current_condition->temp_C,
-           weather->current_condition->winddir16Point, weather->current_condition->windspeedKmph);
+    printf("Region: %s\n", weather->nearest_area->region->value);
+    printf("Date time weather measurements:%s\n", weather->current_condition->localObsDateTime);
+    printf("Weather: %s, air temperature %sC, wind dir %s %s, wind speed %sKmph\n", weather->current_condition->weatherDesc->value, weather->current_condition->temp_C,
+           weather->current_condition->winddir16Point, weather->current_condition->winddirDegree, weather->current_condition->windspeedKmph);
     weather_free(&weather);
     free(buffer);
     }
     if(argc >= 2)
     {
-       system("curl -sLo file7.txt https://wttr.in/params=argv[1]?format=j1");
+       char city[100] = {0};
+       char file_city[100] = {0};
+       sprintf(city, "curl -sLo %s.json https://wttr.in/%s?format=j1", argv[1], argv[1]);
+       sprintf(file_city, "%s.json", argv[1]);
+       if(system(city) != 0)
+       {
+          printf("Error. Failed to establish network connection!\n");
+       }
+       FILE *file = fopen(file_city, "rb");
+
+       if (file == NULL) {
+          fprintf(stderr, "Error: %d: %s\n", errno, strerror(errno));
+          return errno;
+       }
+
+       if (fseek(file, 0, SEEK_END) != 0) {
+          perror("Error seeking file");
+          fclose(file);
+          return errno;
+       }
+       long len = ftell(file);
+       rewind(file);
+
+       char *buffer = (char *) malloc(len + 1);
+       if (buffer == NULL) {
+          fclose(file);
+          perror("Error allocating memory");
+          return ENOMEM;
+       }
+
+       size_t bytes_read = fread(buffer, 1, len, file);
+       if (bytes_read != (size_t) len) {
+         if (feof(file)) {
+            perror("Error: end of file reached before reading expected bytes.");
+            fclose(file);
+            free(buffer);
+            return EIO;
+         } else if (ferror(file)) {
+            perror("Error reading the file");
+            fclose(file);
+            free(buffer);
+            return errno;
+        }
+      }
+      buffer[len] = '\0';
+      fclose(file);
+
+      struct Weather *weather = NULL;
+      nanojsonc_parse_object(buffer, "data", &weather, callback);
+
+      argv[1][0] = toupper(argv[1][0]);
+      if(strstr(weather->nearest_area->region->value, argv[1]) == NULL)
+      {
+          printf("Incorrect city name entered.\n");
+          exit(1);
+      }
+
+      printf("Region: %s\n", weather->nearest_area->region->value);
+      printf("Date time weather measurements:%s\n", weather->current_condition->localObsDateTime);
+      printf("Weather: %s, air temperature %sC, wind dir %s %s, wind speed %sKmph\n", weather->current_condition->weatherDesc->value, weather->current_condition->temp_C,
+           weather->current_condition->winddir16Point, weather->current_condition->winddirDegree, weather->current_condition->windspeedKmph);
+      weather_free(&weather);
+      free(buffer);
     }
-    printf("Hello world!\n");
+    printf("This program exits!\n");
     return 0;
 }
 #endif
